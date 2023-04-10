726d5a1963eeedf155c4db34c03aa5dc4322b71f
mahadinina <mh952219@gmail.com>
2023-04-11 03:22:58 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/alarmandtime/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.alarmandtime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
