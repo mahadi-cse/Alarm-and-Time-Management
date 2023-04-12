a6b4caf93246c7bad6fcd10cdad9fb41f878990b
mahadinina <mh952219@gmail.com>
2023-04-13 00:22:06 +0600

    Alarm fragment designed

---
+++ app/src/main/java/com/example/alarmandtime/Alarm.java
@@ -0,0 +1,22 @@
+package com.example.alarmandtime;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Alarm extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_alarm, container, false);
+        return view ;
+    }
+}
--- app/src/main/java/com/example/alarmandtime/MainActivity.java
+++ app/src/main/java/com/example/alarmandtime/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.alarmandtime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -13,13 +17,47 @@ import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bnView= findViewById(R.id.bottom_nav_view);
+        bnView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id= item.getItemId();
+                if(id==R.id.menu_item_alarm){
+                    loadFrag(new Alarm(),false);
+                }
+                else if(id==R.id.menu_item_world_clock){
+                    loadFrag(new WorldClock(),false);
+                }
+                else if(id==R.id.menu_item_stopwatch){
+                    loadFrag(new Stopwatch(),false);
+                }
+                else {
+                    loadFrag(new Timer(),false);
+                }
 
+                return true;
+            }
+        });
+        bnView.setSelectedItemId(R.id.menu_item_alarm);
+    }
+    public void loadFrag(Fragment fragment , boolean flag ){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(flag){
+            ft.add(R.id.container,fragment);
+        }
+        else{
+            ft.replace(R.id.container,fragment);
+        }
+        ft.commit();
 
     }
+
 }
