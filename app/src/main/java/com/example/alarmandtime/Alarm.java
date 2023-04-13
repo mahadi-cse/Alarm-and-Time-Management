2834958a8a80afb9b98f3a7b0e5861b2accbdcfe
mahadinina <mh952219@gmail.com>
2023-04-13 22:56:05 +0600

    Popu up manu addded

--- app/src/main/java/com/example/alarmandtime/Alarm.java
+++ app/src/main/java/com/example/alarmandtime/Alarm.java
@@ -1,22 +1,69 @@
 package com.example.alarmandtime;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+
+import java.util.Objects;
 
 
 public class Alarm extends Fragment {
 
+   ImageButton more;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_alarm, container, false);
+        more= view.findViewById(R.id.more_option);
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(requireContext(), v);
+
+                // Inflate the menu resource
+                popup.getMenuInflater().inflate(R.menu.user_options, popup.getMenu());
+
+                // Set a click listener for the menu items
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        // Handle menu item clicks
+                        switch (item.getItemId()) {
+                            case R.id.edit:
+                                // Do something
+                                return true;
+                            case R.id.sort:
+                                // Do something
+                                return true;
+                            case R.id.settings:
+                                // Do something
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+
+                // Show the popup menu
+                popup.show();
+            }
+        });
+
         return view ;
     }
 }
